91799fa91f53f6a29c51cb7dc94ecf1ebbcdc367
guilherme-matte <guiguimatte@gmail.com>
2025-05-09 00:29:45 -0300

    att

---
+++ api/src/main/java/CC/CleanCircuit/CleanCircuitApplication.java
@@ -0,0 +1,13 @@
+package CC.CleanCircuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCircuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCircuitApplication.class, args);
+	}
+
+}
---
+++ web/resetarSenha/resetarSenha.js
@@ -0,0 +1,6 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+  //o event listener faz a função ser carregada após o usuario clicar no submit
+    event.preventDefault();
+  let email = document.getElementById("email").value;
+  //codigo para enviar reset de senha por email
+});
